9b651f92599e8ee27c3c271c00d8461e164c9b2e
49035002 <49035002@L815WD220029.edu.cbe.ab.ca>
2025-09-10 13:58:55 -0600

    1:58 PM

--- Chapter3/src/skillBuilders/RectanglePerimeter.java
+++ Chapter3/src/skillBuilders/RectanglePerimeter.java
@@ -4,8 +4,13 @@ public class RectanglePerimeter {
 
 	public static void main(String[] args)
 	{
+		//Initialize variables
+		int width = 4;
+		int length = 13;
 		
-
+		//Calculate and display area
+		int perimeter = width * 2 + length * 2;
+		System.out.print("The perimeter of the rectangle is: " + perimeter);
 
 	}
 
