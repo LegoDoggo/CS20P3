4a536a897eb8f8622c2ae46a191002ff4b8fa9d8
49035002 <49035002@L815WD220029.edu.cbe.ab.ca>
2025-09-11 15:06:00 -0600

    3:05 PM

--- Chapter3/src/skillBuilders/Digits.java
+++ Chapter3/src/skillBuilders/Digits.java
@@ -1,10 +1,22 @@
 package skillBuilders;
 
+import java.util.Scanner;
+
 class Digits {
 
 	public static void main(String[] args)
 	{
+		//Get user input
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a two digit number: ");
+		int userNum = input.nextInt();
 		
+		//Separate the number into 2
+        int tens = userNum / 10;
+        int ones = userNum % 10;
+		
+        System.out.println("Ones-place digit: " + ones);
+        System.out.println("Tens-place digit: " + tens);
 	}
 
 }
