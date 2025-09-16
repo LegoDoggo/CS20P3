03cabff157f5317d9bf3297e688ee0c314886634
49035002 <49035002@L815WD220029.edu.cbe.ab.ca>
2025-09-16 15:33:44 -0600

    3:33 PM

--- Chapter3/src/mastery/Election.java
+++ Chapter3/src/mastery/Election.java
@@ -10,33 +10,39 @@ public class Election {
 		//BC
 		Scanner input = new Scanner(System.in);
 		System.out.println("Elction Results for BC: ");
-		System.out.println("John: ");
+		System.out.print("John: ");
 		int johnVotesBC = input.nextInt();
-		System.out.println("Carrie: ");
+		System.out.print("Carrie: ");
 		int carrieVotesBC = input.nextInt();
 		System.out.println("");
 		
 		//Alberta
 		System.out.println("Elction Results for Alberta: ");
-		System.out.println("John: ");
+		System.out.print("John: ");
 		int johnVotesAB = input.nextInt();
-		System.out.println("Carrie: ");
+		System.out.print("Carrie: ");
 		int carrieVotesAB = input.nextInt();
 		System.out.println("");
 		
 		//Saskatchewan
 		System.out.println("Elction Results for Saskatchewan: ");
-		System.out.println("John: ");
+		System.out.print("John: ");
 		int johnVotesSK = input.nextInt();
-		System.out.println("Carrie: ");
+		System.out.print("Carrie: ");
 		int carrieVotesSK = input.nextInt();
 		System.out.println("");
 				
 		//Calculations
 		int johnTotal = johnVotesBC + johnVotesAB + johnVotesSK;
-		int carrrieTotal= carrieVotesBC + carrieVotesAB + carrieVotesSK;
+		int carrieTotal = carrieVotesBC + carrieVotesAB + carrieVotesSK;
+		int totalVotes = johnTotal + carrieTotal;
+		
+		//Display final results and calculate percentages
 		NumberFormat percent = NumberFormat.getPercentInstance();
-		int johnPercent = percent.format(johnTotal);
+		System.out.format("%-15s %8s %10s %n", "Candidate", "Votes", "Percentage");
+		System.out.format("%-15s %8s %10s %n", "John", johnTotal, percent.format((double)johnTotal / totalVotes));
+		System.out.format("%-15s %8s %10s %n", "Carrie", carrieTotal, percent.format((double)carrieTotal / totalVotes));
+		System.out.format("%-15s %8s %10s %n", "TOTAL VOTES: ", totalVotes, "");
 	}
 
 }
