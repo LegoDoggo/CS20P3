cb2e4f97d804d44125f9138caec392351ee1f1a9
49035002 <49035002@L815WD220029.edu.cbe.ab.ca>
2025-09-22 12:52:59 -0600

    12:49 PM

--- Chapter4/src/skillBuilders/PerfectSquare.java
+++ Chapter4/src/skillBuilders/PerfectSquare.java
@@ -1,5 +1,6 @@
 package skillBuilders;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class PerfectSquare {
 
@@ -12,6 +13,21 @@ public class PerfectSquare {
 		//Get integer
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter an integer: ");
+		int userInt = input.nextInt();
+		
+		//Calculate whether number is perfect square
+		int sqrtInt = (int) Math.sqrt(userInt);
+		sqrtInt *= sqrtInt;
+		
+		if (sqrtInt == userInt)
+		{
+			System.out.print("Your number is a perfect square.");
+		}
+		
+		else
+		{
+			System.out.print("Your number is not perfect square.");
+		}
 		
 		
 
