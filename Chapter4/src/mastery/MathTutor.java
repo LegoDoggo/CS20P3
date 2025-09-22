05f4e44c013f236da780ccf18b309260606b9294
49035002 <49035002@L815WD220029.edu.cbe.ab.ca>
2025-09-22 13:59:21 -0600

    1:59 PM

---
+++ Chapter4/src/mastery/MathTutor.java
@@ -0,0 +1,67 @@
+package mastery;
+import java.util.Scanner;
+
+public class MathTutor {
+
+	public static void main(String[] args) 
+	{
+		//Introduce user
+		System.out.println("Math Tutor");
+		System.out.println("");
+		int answer = 0;
+		String operatorDisplay = "";
+		
+		//Generate random numbers/operators
+		int randomInteger1 = (int)(Math.random() * (10 - 1 + 1) + 1);
+		int randomInteger2 = (int)(Math.random() * (10 - 1 + 1) + 1);
+		int randomOperator = (int)(Math.random() * (4 - 1 + 1) + 1);
+		
+		
+		//Determine answer and operator to display
+		switch(randomOperator)
+		{
+		
+		case 1:
+			answer = randomInteger1 + randomInteger2;
+			operatorDisplay = "+";
+			break;
+			
+		case 2:
+			answer = randomInteger1 - randomInteger2;
+			operatorDisplay = "-";
+			break;
+			
+		case 3:
+			answer = randomInteger1 * randomInteger2;
+			operatorDisplay = "*";
+			break;
+			
+		case 4:
+			answer = randomInteger1 / randomInteger2;
+			operatorDisplay = "/";
+			break;
+		
+		}
+		
+		//Ask question to user and get input
+		System.out.print("What is " + randomInteger1 + operatorDisplay + randomInteger2 + "?");
+		Scanner input = new Scanner(System.in);
+		System.out.println("");
+		System.out.print("Answer: ");
+		double userAns = input.nextInt();
+		System.out.println("");
+		
+		//Determine whether answer is right or wrong
+		if (userAns == answer)
+		{
+			System.out.println("Correct!");
+		}
+		
+		else
+		{
+			System.out.println("Incorrect! The answer is " + answer);
+		}
+
+	}
+
+}
