9b6a6e2288180df186f32e25b88cc4d560289fdc
49035002 <49035002@L815WD220029.edu.cbe.ab.ca>
2025-10-29 13:46:02 -0600

    1:45 PM

--- Chapter5/src/mastery/Password.java
+++ Chapter5/src/mastery/Password.java
@@ -1,3 +1,16 @@
+/*
+
+Program: Password.java          Last Date of this Revision: October 29, 2025
+
+Purpose: An application designed to store a password, and then prompt the user for the password for login. The program will determine whether the password is correct or not and whether to grant access to the user.
+
+Author: Easton Seeley 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
 package mastery;
 import java.util.Scanner;
 
@@ -7,6 +20,7 @@ public class Password {
 	{
 		//Initialize variable for loop
 		int attempts = 0;
+		boolean success = false;
 		
 		//Prompt user to make password
 		Scanner input = new Scanner(System.in);
@@ -15,24 +29,55 @@ public class Password {
 		System.out.println("");
 		
 		//Prompt user for password
-		while (attempts <= 3)
+		while (attempts < 3)
 		{
 			System.out.print("Enter the password: ");
 			String userInput = input.next();
 			
-			if (userInput == password)
+			if (userInput.equals(password))
 			{
 				System.out.println("Welcome.");
+				success = true;
+				break;
 			}
 			
 			else
 			{
-				
+				System.out.println("The password you typed is incorrect.");
+				attempts ++;
 			}
 		}
 		
+		if (success == false)
+		{
+			System.out.println("Access denied.");
+		}
 		
 
 	}
 
 }
+
+/* Screen Dump
+
+Create a password: er2
+
+Enter the password: er3
+The password you typed is incorrect.
+Enter the password: erf
+The password you typed is incorrect.
+Enter the password: er2
+Welcome.
+
+Create a password: per
+
+Enter the password: ert
+The password you typed is incorrect.
+Enter the password: dfd
+The password you typed is incorrect.
+Enter the password: erty
+The password you typed is incorrect.
+Access denied.
+
+ 
+ */
