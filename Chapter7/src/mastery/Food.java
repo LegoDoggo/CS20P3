4a0ee6b11e8da39166f619441a59b53a2a74cdb4
49035002 <49035002@L815WD220029.edu.cbe.ab.ca>
2025-12-01 13:50:02 -0700

    1:49 PM

--- Chapter7/src/mastery/Food.java
+++ Chapter7/src/mastery/Food.java
@@ -26,16 +26,69 @@ public class Food
 		fiber = fi; 
 	}
 	
-	//Accessor method
-	public double getPrice()
+	//Accessor methods
+	
+	//Order price
+	public double getOPrice()
 	{
-		return orderPrice;
+		return Math.round(orderPrice * 100) / 100.0;
 	}
 	
-	//Modifier method
-	public void setPrice(double op)
+	//Food price
+	public double getFPrice()
+	{
+		return price;
+	}
+	
+	//Fat
+	public double getFat()
+	{
+		return fat;
+	}
+	
+	//Carbs
+	public double getCarbs()
+	{
+		return carbs;
+	}
+	
+	//Fiber
+	public double getFiber()
+	{
+		return fiber;
+	}
+	
+	
+	//Modifier methods
+	
+	//Order price
+	public void setOPrice(double op)
 	{
 		orderPrice = op;
+	}
+	
+	//Food price
+	public void setFPrice(double pr)
+	{
+		price = pr;
+	}
+	
+	//Fat
+	public void setFat(double fa)
+	{
+		fat = fa;
+	}
+	
+	//Carbs
+	public void setCarbs(double ca)
+	{
+		carbs = ca;
+	}
+	
+	//Fiber
+	public void setFiber(double fi)
+	{
+		fiber = fi;
 	}
 	
 	
--- Chapter7/src/mastery/LunchOrder.java
+++ Chapter7/src/mastery/LunchOrder.java
@@ -1,12 +1,31 @@
 package mastery;
+import java.util.Scanner;
 
 public class LunchOrder {
 
 	public static void main(String[] args) 
 	{
-
+		//Food objects (contains food info)
+		Food burger = new Food(1.85, 9.0, 33.0, 1.0);
+		Food salad = new Food(2.00, 1.0, 11.0, 5.0);
+		Food fries = new Food(1.30, 11.0, 36.0, 4.0);
+		Food soda = new Food(0.95, 0.0, 38.0, 0.0);
 		
-
+		//Get user input and print nutritional information
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter number of hamburgers: ");
+		int numBurg = input.nextInt();
+		System.out.println("");
+		
+		System.out.print("Enter number of salads: ");
+		int numSal = input.nextInt();
+		
+		System.out.print("Enter number of fries: ");
+		int numFry= input.nextInt();
+		
+		System.out.print("Enter number of sodas: ");
+		int numSoda = input.nextInt();
 
 	}
 
